2b023d6f736744f9142a55067daaf29e794629b3
K3ND4LL <kendal.chgz14@gmail.com>
2026-08-16 10:06:30 -0600

    Calculadora_0.0.3

--- src/main/java/com/mycompany/practica2/Calculadora.java
+++ src/main/java/com/mycompany/practica2/Calculadora.java
@@ -9,6 +9,10 @@ package com.mycompany.practica2;
  * @author kenda
  */
 public class Calculadora extends javax.swing.JFrame {
+    
+    public float num1;
+    public float num2;
+    public String accion;
    
     private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(Calculadora.class.getName());
 
@@ -202,11 +206,15 @@ public class Calculadora extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
+         this.num1=Float.parseFloat(this.Resultado.getText());
+        this.accion="*";
+        this.Resultado.setText(" ");
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
+         this.num1=Float.parseFloat(this.Resultado.getText());
+        this.accion="-";
+        this.Resultado.setText(" ");
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
@@ -226,7 +234,9 @@ public class Calculadora extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton11ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-        // TODO add your handling code here:
+         this.num1=Float.parseFloat(this.Resultado.getText());
+        this.accion="/";
+        this.Resultado.setText(" ");
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
@@ -234,7 +244,7 @@ public class Calculadora extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton5ActionPerformed
 
     private void jButton8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton8ActionPerformed
-        this.Resultado.setText(this.Resultado.getText()+"5");
+        this.Resultado.setText(" ");
     }//GEN-LAST:event_jButton8ActionPerformed
 
     private void jButton9ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton9ActionPerformed
@@ -250,11 +260,19 @@ public class Calculadora extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton17ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+        this.num1=Float.parseFloat(this.Resultado.getText());
+        this.accion="+";
+        this.Resultado.setText(" ");
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton16ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton16ActionPerformed
-        // TODO add your handling code here:
+        this.num2=Float.parseFloat(this.Resultado.getText());
+        switch(this.accion){
+            case "+":this.Resultado.setText(Float.toString(this.num1+this.num2));break;
+            case "-":this.Resultado.setText(Float.toString(this.num1-this.num2));break;
+            case "*":this.Resultado.setText(Float.toString(this.num1*this.num2));break;
+            case "/":this.Resultado.setText(Float.toString(this.num1/this.num2));break;
+        }
     }//GEN-LAST:event_jButton16ActionPerformed
 
     private void jButton14ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton14ActionPerformed
